3647df5d042312e681b768a7d8faabad331e6560
Abhishek <abhishekonline45@gmail.com>
2024-03-12 16:58:34 +0530

    first commit

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-Let menu = document.querySelector('#menu-icon');
-Let navlist = document.querySelector('.navlist');
+// let menu = document.querySelector('#menu-icon');
+// let navlist = document.querySelector('.navlist');
 
-menu.onclick = () => {
-   menu.classList.toggle('bx-x');
-   navlist.classList.toggle('open');
+
+// menu.onclick = ()=> {
+//    menu.classList.toggle('bx-x');
+//    navlist.classList.toggle('open');
+// };
+
+let menu=document.querySelector('#menu-icon');
+let navlist=document.querySelector('.navlist');
+
+menu.onclick=()=>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+
 };
 
-const sr = scrollreveal ({
+
+const sr = ScrollReveal ({
     distance: "65px",
     duration: 2600,
     dealy: 450,
     reset: true
 });
 
-sr.reveal('.hero-text',{dealy:200,origin:'top'});
-sr.reveal('.hero-img',{dealy:450,origin:'top'});
-sr.reveal('.icons',{dealy:500,origin:'left'});
-sr.reveal('.scroll-down',{dealy:500,origin:'right'});
+ScrollReveal().reveal('.hero-text',{dealy:200,origin:'top'});
+ScrollReveal().reveal('.hero-img',{dealy:450,origin:'top'});
+ScrollReveal().reveal('.icons',{dealy:500,origin:'left'});
+ScrollReveal().reveal('.scroll-down',{dealy:500,origin:'right'});
